33666c5b7d4c8ca4280668069ce6668aa6d54b05
LoafyITBum <122436356+LoafyITBum@users.noreply.github.com>
2024-05-06 21:06:37 +0800

    firts php program

---
+++ test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<?php
+        $row = 1;
+if (($handle = fopen("14.website_sessions_2014_Jan.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        echo "<tr>";
+        for ($c=0; $c < $num; $c++) {
+            echo "<td>" . htmlspecialchars($data[$c]) . "</td>";
+        }
+        echo "</tr>";
+        $row++;
+    }
+    fclose($handle);
+}
+        ?>
+</body>
+</html>
